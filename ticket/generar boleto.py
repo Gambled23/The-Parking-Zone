e4cbd7157be20fa619160980cdb4b747488ad5d5
Gambled23 <ipog71@gmail.com>
2023-03-26 14:56:36 -0600

    generar qr

--- ticket/generar boleto.py
+++ ticket/generar boleto.py
@@ -1,5 +1,6 @@
 import jinja2
 import pdfkit
+import qrcode
 from datetime import datetime
 
 def generarPDF ():
@@ -10,14 +11,18 @@ def generarPDF ():
     context = {'my_name': my_name,'today_date': today_date, 'file_path': file_path}
     template_loader = jinja2.FileSystemLoader('./')
     template_env = jinja2.Environment(loader=template_loader)
-    template = template_env.get_template('boleto.html')
+    template = template_env.get_template('ticket/boleto.html')
     output_text = template.render(context)
     template_loader = jinja2.FileSystemLoader('./')
     template_env = jinja2.Environment(loader=template_loader)
-    template = template_env.get_template('boleto.html')
+    template = template_env.get_template('ticket/boleto.html')
     output_text = template.render(context)
     config = pdfkit.configuration(wkhtmltopdf='C:\Program Files\wkhtmltopdf\\bin\wkhtmltopdf.exe')
     pdfkit.from_string(output_text, './ticket/ticket.pdf', configuration=config, css='ticket\style.css', options={"enable-local-file-access": ""})
 
+def generarQR(data):
+    img = qrcode.make(data)
+    img.save('./ticket/qrticket.png')
 
+generarQR('A-22')
 generarPDF()
