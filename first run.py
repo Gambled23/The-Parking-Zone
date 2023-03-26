765d6ec62acfd7c96e18d5080d2d8647ba20e37f
Gambled23 <ipog71@gmail.com>
2023-03-26 11:06:21 -0600

    ignore

--- first run.py
+++ first run.py
@@ -1,14 +1,14 @@
 from configuration_scripts.scripts import createDatabase, habilitarCajones, crearAdmin
 
 #Crear base de datos y tablas
-#createDatabase()
+createDatabase()
 
 #Crear cajones
 print('Bienvenido a la instalacion de The Parking Zone, deberá ingresar unos datos antes del primer uso\n')
 print('Los estacionamientos se componen de filas y columnas (A-21, C-12, X-03, etc)')
 letra = input("Ingresa la ultima letra del estacionamiento: ")
 numero = int(input("Ingresa el ultimo número del estacionamiento: "))
-#habilitarCajones(letra, numero)
+habilitarCajones(letra, numero)
 
 print('Para ingresar al panel de administrador será necesario crear un usuario y una contraseña, no olvides estos datos ni los compartas con nadie externo')
 usuario = input('Ingresa un usuario para el administrador:')
