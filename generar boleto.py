3eab4fc2c7b5d091a320654e1ff61ea36bcd0576
Gambled23 <ipog71@gmail.com>
2023-03-29 15:04:04 -0600

    el cajon se marca como ocupado despues de generar el PDF

--- generar boleto.py
+++ generar boleto.py
@@ -4,34 +4,39 @@ import qrcode
 from datetime import datetime
 import algoritmo_asignacion
 
+def actualizarCajon(cajon):
+    cursor = algoritmo_asignacion.conectar();
+    sql = ''f"UPDATE cajon SET ocupado = True WHERE id_cajon = '{cajon[0]}'"'';
+    cursor.execute(sql)
+    cursor.close()
+
+#GenerarPDF requiere saber si es de discapacitados o no
 def generarPDF (discapacitado):
     fecha = datetime.today().date()
     hora = f'{datetime.today().hour}:{datetime.today().minute}'
-    file_path = 'H:\Codigos\The Parking Zone\qrticket.png'
-    #obtener cajon asignado
+    #generar qr
     cajon = algoritmo_asignacion.obtenerCajon(discapacitado)
     letra = cajon[1].upper()
     numero = cajon[2]
+    generarQR(cajon)
+    file_path = 'H:\Codigos\The Parking Zone\qrticket.png'
     context = {'fecha': fecha, 'hora': hora, 'file_path': file_path, 'letra':letra, 'numero':numero}
 
-    template_loader = jinja2.FileSystemLoader('./')
-    template_env = jinja2.Environment(loader=template_loader)
-    template = template_env.get_template('ticket/boleto.html')
-    output_text = template.render(context)
+    #Toda la madre para generar el pdf
     template_loader = jinja2.FileSystemLoader('./')
     template_env = jinja2.Environment(loader=template_loader)
     template = template_env.get_template('ticket/boleto.html')
     output_text = template.render(context)
     config = pdfkit.configuration(wkhtmltopdf='C:\Program Files\wkhtmltopdf\\bin\wkhtmltopdf.exe')
     pdfkit.from_string(output_text, './ticket/ticket.pdf', configuration=config, css='ticket\style.css', options={"enable-local-file-access": ""})
+    
+    actualizarCajon(cajon)
 
-def generarQR(discapacitado):
-    cajon = algoritmo_asignacion.obtenerCajon(discapacitado)
+def generarQR(cajon):
     letra = cajon[1].upper()
     numero = cajon[2]
     data = f'{letra}{numero}'
     img = qrcode.make(data)
     img.save('./ticket/qrticket.png')
 
-generarQR(True)
-generarPDF(True)
+generarPDF(False)
