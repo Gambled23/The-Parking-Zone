b7423d3a04c69faaf470f1260ef2745a4b677b18
Gambled23 <ipog71@gmail.com>
2023-04-28 10:35:23 -0600

    funcion para mostrar cajones inconsistentes

--- sensores.py
+++ sensores.py
@@ -1,6 +1,7 @@
 import serial
 import time
 import tkinter as tk
+import comprobarSensores
 
 ser = serial.Serial('COM4', 9600, timeout = 1)
 time.sleep(2)
@@ -14,7 +15,10 @@ def tecla(evento):
         datos.append(int(ser.read()))
 
     ser.read()
-    print(datos)
+
+    #Datos es 1 si está ocupado, 0 si se desocupa
+    comprobarSensores.comprobarCajon(datos)
+
 
 raiz = tk.Tk()
 raiz.bind('<t>', tecla)
