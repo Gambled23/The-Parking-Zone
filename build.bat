e0cb8a0c10ea04fe3e54c11ac1df2d4bb45e88e3
Gambled23 <ipog71@gmail.com>
2023-05-21 15:17:00 -0600

    build script

---
+++ build.bat
@@ -0,0 +1,5 @@
+pyinstaller --onefile -w '.\first run.py'
+pyinstaller --onefile -w .\intAdm.pyw   
+pyinstaller --onefile -w .\lector_cajones.py 
+pyinstaller --onefile -w .\lector_salida.py  
+pyinstaller --onefile -w .\main.py           
