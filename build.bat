9384e8125e390341ec465fcf96d52645aeca9e23
Gambled23 <ipog71@gmail.com>
2023-05-22 16:17:50 -0600

    build

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
-pyinstaller --onefile -w '.\first run.py'
-pyinstaller --onefile -w .\intAdm.pyw   
-pyinstaller --onefile -w .\lector_cajones.py 
-pyinstaller --onefile -w .\lector_salida.py  
-pyinstaller --onefile -w .\main.py           
+rm *.spec
+rm ./dist/*.exe
+
+pyinstaller --onefile -w --icon=panel_admin\images\iconoAzul.ico '.\first run.py'
+pyinstaller --onefile -w --icon=panel_admin\images\icono.ico .\intAdm.pyw   
+pyinstaller --onefile -w --icon=panel_admin\images\iconoAzul.ico .\lector_cajones.py 
+pyinstaller --onefile -w --icon=panel_admin\images\iconoAzul.ico .\lector_salida.py  
+pyinstaller --onefile -w --icon=panel_admin\images\icono.ico .\main.py           
